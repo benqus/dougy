42d0224259a126a8befaea77c441c661ed230f07
Bence <ben.kormos@gmail.com>
2013-12-30 17:21:36 -0500

    Implements reset, clone, filter, each, attributes (#2, #3, #4, #5, #7)

--- js/modules/core/model.js
+++ js/modules/core/model.js
@@ -8,17 +8,11 @@
   var isObject = utils.isObject;
   var isDefined = utils.isDefined;
   
-  // base (super)
-  var base = dougy.component;
-
-  // base#destroy
-  var destroy = base.destroy;
-  
   /**
    * @class model
    * @extends {dougy.component}
    */
-  dougy.model = base.extend(function ($model, attrs) {
+  dougy.model = dougy.component.extend(function ($model, attrs) {
     /**
      * @private
      * @type {Object}
@@ -89,11 +83,99 @@
     };
     
     /**
+     * Returns a plain Object clone of the model's attributes.
+     * @returns {Object}
+     */
+    $model.attributes = function () {
+      return extend({}, attributes);
+    };
+    
+    /**
+     * Filters the model's attributes based on the provided comparator method.
+     * 
+     * Example:
+     *    var model = dougy.model.create({
+     *      "a": 1,
+     *      "b": 2,
+     *      "c": 3,
+     *      //...
+     *    });
+     * 
+     *    var filtered = model.filter(function (key, value) {
+     *      return (key === 'a' || value < 3);
+     *    });
+     * 
+     * @param {Function} comparator
+     * @returns {Object}
+     */
+    $model.filter = function (comparator) {
+      var filtered = {};
+      
+      this.each(function (key, value) {
+        if (comparator.apply($model, arguments) === true) {
+          filtered[key] = value;
+        }
+      });
+      
+      return filtered;
+    };
+    
+    /**
+     * Iterates over the model's attributes and ivokes
+     * the provided method for each attribute.
+     * 
+     * Example:
+     *    var model = dougy.model.create({
+     *      "a": 1,
+     *      "b": 2,
+     *      "c": 3,
+     *      //...
+     *    });
+     * 
+     *    model.each(function (key, value, attributes) {
+     *      console.log(key, value, attributes);
+     *    });
+     * 
+     * @param {Function} iterator
+     * @returns {dougy.model}
+     */
+    $model.each = function (iterator) {
+      var i;
+      
+      for (i in attributes) {
+        if (attributes.hasOwnProperty(i)) {
+          iterator.call(this, i, attributes[i], attributes);
+        }
+      }
+      
+      return this;
+    };
+    
+    /**
+     * Returns a new dougy.model instance with the same attribute values.
+     * @returns {dougy.model}
+     */
+    $model.clone = function () {
+      return dougy.model.create(attributes);
+    };
+    
+    /**
+     * Resets the model instance.
+     * @returns {dougy.model}
+     */
+    $model.reset = function () {
+      attributes = {};
+      return this;
+    };
+    
+  }, {
+    
+    /**
      * @override
      */
-    $model.destroy = function () {
-      attributes = {};
-      return destroy.apply(this, arguments);
+    "destroy": function () {
+      this.reset();
+      return this.base.destroy.apply(this, arguments);
     }
   });
   
